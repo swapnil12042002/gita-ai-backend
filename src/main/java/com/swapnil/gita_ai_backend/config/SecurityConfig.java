8a8b0f64fee6cf95879419619dedb44bbcc8f098
Swapnil Verma <vermaswapnil1296@gmail.com>
2026-08-19 21:25:40 +0530

    backend changes

--- src/main/java/com/swapnil/gita_ai_backend/config/SecurityConfig.java
+++ src/main/java/com/swapnil/gita_ai_backend/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
